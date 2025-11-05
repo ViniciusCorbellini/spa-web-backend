841b210f02bbb2a6ebb9ce39a249b862fc4d7ff9
ManoCorbas <corbellinivinicius4@gmail.com>
2025-11-04 22:43:24 -0300

    cors config

---
+++ src/main/java/com/manocorbas/dev_web_backend/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.manocorbas.dev_web_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permite CORS para todas as rotas
+                        .allowedOrigins("http://localhost:5173") // A origem do seu front-end
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                        .allowedHeaders("*") // Permite todos os cabeçalhos
+                        .allowCredentials(true); // Permite envio de cookies (se necessário)
+            }
+        };
+    }
+}  
